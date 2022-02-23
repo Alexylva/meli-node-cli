e2bd08fb87f24031d993aa452c6afa1ec4e0f848
Alexylva <nicholas.roncally@gmail.com>
2022-02-23 10:36:48 -0300

    Switched stupid then-tree for awaits
    Didn't knew I could just do that.

--- app.js
+++ app.js
@@ -4,14 +4,9 @@ const mlApi = require('./mlApi');
 const http = require('./http');
 const repl = require('./repl')
 
-env.setup().then(() => {
-    sessionApi.setup(env.session_path).then(() => {
-        mlApi.setup(sessionApi.getAccessToken, sessionApi.setAccessToken, env.getAppKeys).then(() => {
-            http.setup(mlApi, sessionApi).then(() => {
-                repl.setup(sessionApi, mlApi)
-            })
-        })
-    })
+env.setup().then(async () => {
+    await sessionApi.setup(env.session_path);
+    await mlApi.setup(sessionApi.getAccessToken, sessionApi.setAccessToken, env.getAppKeys);
+    await http.setup(mlApi, sessionApi);
+    await repl.setup(sessionApi, mlApi);
 })
-
-
