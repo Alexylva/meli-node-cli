c25af2a94d5b37d36567b30ea3aae6609a7c5c28
Alexylva <nicholas.roncally@gmail.com>
2022-03-07 17:09:03 -0300

    Rehoming variables from mlapi to http

--- http.js
+++ http.js
@@ -2,36 +2,48 @@
  * Server Configurations
 **/
 
-function setup(mlApi, sessionApi) {
+let auth_url;
+
+function setup(accessTokenFetcher, sessionApi, appKeysGetter) {
   return new Promise(resolve => {
     const express = require("express");
     const http = express();
 
+    const [fetchAccessToken, getAppKeys] = [accessTokenFetcher, appKeysGetter];
+
+    const hostname = 'localhost';
+    const port = 63771; //🤔 Perhaps choose port at random?
+    const server_url = `http://${hostname}:${port}/`
+    const auth_resource = `code`;
+    const redirect_uri = `${server_url}${auth_resource}`;
+    auth_url = `https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=${getAppKeys().app_id}&redirect_uri=${redirect_uri}`;
+
     http.use(express.json());
     http.use(express.urlencoded({ extended: true }));
 
-    http.get(`/${mlApi.auth_resource}`, (req, res, next) => {
+    http.get(`/${auth_resource}`, (req, res) => {
       let auth = req.query.code;
-      if (!auth) return onErr("Fail retrieving Auth Code", auth);
+      if (!auth) return onErr("Failed retrieving Auth Code", auth);
       console.log(`Auth Code: ${auth} for slot "${sessionApi.getProfile()}"`, true);
       sessionApi.setAuth(auth);
       res.send("<script>window.close()</script>");
 
-      mlApi.fetchAccessToken(auth).then((data) => {
-        console.log(`Access Token: ${data.access_token}`, true);
-        sessionApi.setAccessToken(data.access_token, data);
+      let response = fetchAccessToken(auth, redirect_uri).then((response)=>{;
+        console.log(response);
+        console.log(`Access Token: ${response.access_token}`, true);
+        sessionApi.setAccessToken(response.access_token, response)
       });
-      
     });
 
-    http.listen(mlApi.port, () => {
-      console.log(`Server running on ${mlApi.server_url}`, true);
-      if (!sessionApi.hasAccessToken()) console.log(`Use this link to get the token ${mlApi.auth_url}`);
+    http.listen(port, () => {
+      console.log(`Server running on ${server_url}`, true);
+      sessionApi.verifyAccessToken(auth_url);
       resolve(http);
     });
   })
 }
 
 module.exports = {
-  setup
+  setup,
+  auth_url
 }
