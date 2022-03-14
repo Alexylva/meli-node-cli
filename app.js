488f3f37d130021756211ce040ecadc2c9651bec
Alexylva <nicholas.roncally@gmail.com>
2022-03-14 17:22:58 -0300

    Add license

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+/**
+ * Meli Node CLI is a command-line implementation of Mercado Livre API.
+ * Copyright (C) 2022  Alexylva
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 
 function main(args) {
     const env = require('./env');
